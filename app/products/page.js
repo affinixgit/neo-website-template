803308391b1b2c870f07bf2259817b1dabb807a9
swapnil <swapnil@classhubapp.com>
2024-10-30 22:20:02 +0530

    product grid listing completed

--- app/products/page.js
+++ app/products/page.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+
 import Link from "next/link";
 import Image from "next/image";
+import { events } from "@/lib/dummydata";
 
-// Api data
-import { cmsApiBaseUrl } from "@/data/token/TokenJsonP"; // Adjust path as necessary
+
 import bannerImg from "@/public/images/banner/banner3.jpg"; // Update image import
 
 function trimText(text) {
@@ -17,15 +17,9 @@ function getText(text) {
   return text?.replace(/<[^>]+>/g, "") || "";
 }
 
-const Products = ({ data, mediaUrl }) => {
-  const url = mediaUrl?.indexPage?.mediaBaseUrl;
-  const [products, setProducts] = useState([]);
+const Products = () => {
 
-  useEffect(() => {
-    if (data) {
-      setProducts(data);
-    }
-  }, [data]);
+
 
   return (
     <div className="page-content">
@@ -56,7 +50,7 @@ const Products = ({ data, mediaUrl }) => {
             <div className="row">
               <div className="col">
                 <div className="row">
-                  {products.map((item, idx) =>
+                  {events.map((item, idx) =>
                     item.enabled === 1 ? (
                       <div className="col-md-6 col-lg-4 col-sm-6 m-b30" key={idx}>
                         <div className="cours-bx">
@@ -65,8 +59,8 @@ const Products = ({ data, mediaUrl }) => {
                               className="img-fluid"
                               width={450}
                               height={300}
-                              src={`${url}webCourse/${item?.eventImage}`}
-                              alt={item?.eventTitle}
+                              src={`${item?.productImage}`}
+                              alt={item?.productTitle}
                             />
                             <Link href={`/products/${item?.productId}`} className="btn">
                               Read More
@@ -75,7 +69,7 @@ const Products = ({ data, mediaUrl }) => {
                           <div className="info-bx">
                             <h6>
                               <Link href={`/products/${item?.productId}`}>
-                                {item?.eventTitle}
+                                {item?.productTitle}
                               </Link>
                             </h6>
                             <span>{trimText(getText(item?.description))}</span>
@@ -94,38 +88,6 @@ const Products = ({ data, mediaUrl }) => {
   );
 };
 
-export async function getServerSideProps(context) {
-  const domain = context.req.headers.host;
 
-  let resData = [];
-  let mediaUrl = {};
-
-  try {
-    const response = await fetch(`${cmsApiBaseUrl}Domain/OfflineCourses/${domain}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch offline products data");
-    }
-    resData = await response.json();
-  } catch (error) {
-    console.error("Error fetching offline products data:", error);
-  }
-
-  try {
-    const mediaUrlRes = await fetch(`${cmsApiBaseUrl}Domain/Index/${domain}`);
-    if (!mediaUrlRes.ok) {
-      throw new Error("Failed to fetch media URL");
-    }
-    mediaUrl = await mediaUrlRes.json();
-  } catch (error) {
-    console.error("Error fetching media URL:", error);
-  }
-
-  return {
-    props: {
-      data: resData,
-      mediaUrl: mediaUrl,
-    },
-  };
-}
 
 export default Products;
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: [
       "images.ctfassets.net",
       "placeholder.com",
+      "bucket-chat-classhub.s3.ap-south-1.amazonaws.com"
     ],
   },
 };
