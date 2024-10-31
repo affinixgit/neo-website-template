decbc3d1f72141de1ae05b63de6c6d0bd2d61a4f
swapnil <swapnil@classhubapp.com>
2024-10-31 17:03:47 +0530

    category wise product listing completed

---
+++ app/category/page.js
@@ -0,0 +1,66 @@
+
+import Link from "next/link";
+import Image from "next/image";
+import bannerImg from "@/public/images/banner/banner3.jpg"; // Update image import
+import { getCategories } from "@/lib/category";
+import { categories } from "@/lib/dummydata";
+
+
+
+export default async function CategoriesPage() {
+
+    const productItem = await getCategories();
+
+    return (
+        <div className="page-content">
+            <div
+                className="page-banner ovbl-dark"
+                style={{ backgroundImage: `url(${bannerImg.src})` }} // Using imported image
+            >
+                <div className="container">
+                    <div className="page-banner-entry">
+                        <h1 className="text-white">Product Categories</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="breadcrumb-row">
+                <div className="container">
+                    <ul className="list-inline">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>Product Categories</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="content-block">
+                <div className="section-area section-sp5">
+                    <div className="container">
+                       
+
+                                <div className="category-tags" >
+                                    {categories
+                                        .map((category, idx, arr) => (
+                                           
+                                                <span key={category.idCategory}>
+                                                    <Link href={`/category/${category.slug}`} className="category-link">
+                                                        {category.categoryTitle}
+                                                    </Link>                                                
+                                                </span>
+                                     
+                                        ))}
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    );
+};
+
+
+
--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import HTMLReactParser from "html-react-parser";
 import { getProductItem } from '@/lib/product';
+import ProductDescription from '@/components/product/productDescription';
 
 export default async function ProductDetailPage({ params }) {
     const { productId } = await params;
@@ -43,75 +44,20 @@ export default async function ProductDetailPage({ params }) {
                     </div>
                 </div>
 
-                {/* Content Block */}
-                <div className="content-block">
-                    <div className="section-area section-sp5">
-                        <div className="container">
-                            <div className="row d-flex flex-row">
-                                {/* Left Column - Product Details */}
-                                <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
-                                    <div className="courses-post">
-                                        <div className="ttr-post-media media-effect">
-                                            <Image
-                                                src={`${productItem?.media.mediaBaseUrl}/${productItem?.media.fileSlug}`}
-                                                alt={productItem.altText}
-                                                width={400}
-                                                height={300}
-                                                loading="lazy" // Optimize loading
-                                            />
-                                        </div>
+                <ProductDescription productItem={productItem}></ProductDescription>
 
-                                    </div>
-                                </div>
-
-                                {/* Right Column - Additional Content or Sidebar (if needed) */}
-                                <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-                                    {/* Additional content like related products, reviews, etc. can go here */}
-
-                                    <div className="ttr-post-info m-b30">
-                                        <div className="ttr-post-title">
-                                            <h2 className="post-title">{productItem.productTitle}</h2>
-                                        </div>
-                                        <div className="ttr-post-text">
-                                            <p>{productItem.description}</p>
-                                        </div>
-
-
-                                        {/* Brochure Link */}
-                                        {productItem.brochureLink && (
-                                            <Link href={productItem.brochureLink} target="_blank" rel="noopener noreferrer" className="product-btn">
-                                                DOWNLOAD BROCHURE
-                                            </Link>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-area section-sp5">
-                                <div className="row d-flex flex-row">
-                                    {/* Product Details */}
-                                    <div className="product-details">
-                                        <h3>Product Details</h3>
-                                        {HTMLReactParser(productItem.productDetails)}
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <br></br>
-                        <div className="container">
-                            <h4>Filter Products</h4>
-                            <div className="product-tags" >
-                                {productItem.tags
-                                    .map((tag, tagIdx, arr) => (
-                                        <span key={tagIdx}>
-                                            <Link href={`/tags/${tag.slug}/${productItem.slug}`} className="tag-link">
-                                                {tag.tagName}
-                                            </Link>
-                                            {tagIdx < arr.length - 1 && " | "}
-                                        </span>
-                                    ))}
-                            </div>
-                        </div>
+                <div className="container">
+                    <h4>Filter Products</h4>
+                    <div className="product-tags" >
+                        {productItem.tags
+                            .map((tag, tagIdx, arr) => (
+                                <span key={tagIdx}>
+                                    <Link href={`/tags/${tag.slug}/${productItem.slug}`} className="tag-link">
+                                        {tag.tagName}
+                                    </Link>
+                                    {tagIdx < arr.length - 1 && " | "}
+                                </span>
+                            ))}
                     </div>
                 </div>
             </div>
