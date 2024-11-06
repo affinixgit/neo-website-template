6ba0b918c978f7991c8691185e95f8a502259303
swapnil <swapnil@classhubapp.com>
2024-11-06 17:54:15 +0530

    completed footer sample

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'; // Font Awesome CSS
 import '../lib/fontawesome'; // Your Font Awesome configuration
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainHeader from "@/components/Header/main-header";
-
-
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,40 +17,32 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-
 const requestOptions = {
   method: "GET",
-  redirect: "follow"
+  redirect: "follow",
 };
 
 const response = await fetch("http://localhost:3006/api/v1/navMenu", requestOptions);
 
 if (!response.ok) {
-  throw new Error('Failed to fetch services');
+  throw new Error("Failed to fetch services");
 }
 
 const headerData = await response.json();
+
 export const metadata = {
   title: headerData.businessInfo.businessName,
   description: headerData.businessInfo.businessDescription,
 };
 
-// Async function to fetch menu data from your server or API
-// async function getMenuData() {
-//   const response = await fetch('https://your-api-url.com/menu'); // Replace with your API endpoint
-//   if (!response.ok) {
-//     throw new Error('Failed to fetch menu data');
-//   }
-//   return response.json();
-// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <MainHeader headerData ={headerData}></MainHeader>
+        <MainHeader headerData={headerData} />
         <main>{children}</main>
-        
+        <Footer footerData={headerData} /> {/* Footer with data */}
       </body>
     </html>
   );
