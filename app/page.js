8669020c3d7801042705d4cdd7a2a4a7e66c4c5a
swapnil <swapnil@classhubapp.com>
2024-11-07 18:20:31 +0530

    update meta data and headers color schema

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Slider1 from "@/components/carousal/slider";
 import ServiceSlider from "@/components/service/serviceSlider";
+import Head from "next/head";
+
+
+
 
 export default async function Home() {
 
@@ -9,17 +13,25 @@ export default async function Home() {
     redirect: "follow"
   };
 
-  const response = await fetch("http://localhost:3006/api/v1/service?pageNumber=1&pageSize=5", requestOptions);
-  
+  const response = await fetch("http://localhost:3006/api/v1/service?pageNumber=1&pageSize=5", requestOptions);  
   if (!response.ok) {
     throw new Error('Failed to fetch services');
   }
 
   const productResponse = await response.json();
   
+  const websiteResponse = await fetch("http://localhost:3006/api/v1/website", requestOptions);
+  if (!response.ok) {
+    throw new Error('Failed to fetch website Data');
+  }
+  const websiteData = await websiteResponse.json();
 
+
+
+  
   return (
     <>
+    
       <div className="page-content bg-white">
         <Slider1></Slider1>
         <div className="section-area section-sp5">
@@ -27,6 +39,10 @@ export default async function Home() {
            <ServiceSlider  products={productResponse.service} />              
           </div>
         </div>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: websiteData.jsonLd}}
+        />
       </div>
     </>
   );
