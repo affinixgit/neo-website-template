4a657d99f346ff0a638ff9109238ea07f595ec63
swapnil <swapnil@classhubapp.com>
2024-11-19 12:37:55 +0530

    updated config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // config/config.js
 const config = {
   apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3006/api/v1",
+    process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.affinixdigital.com/api/v1",
   subscriptionId:
     process.env.NEXT_PUBLIC_SUBSCRIPTION_ID ||
     "a990fb30-7621-4cea-926a-b5ad5d6ea5ef",
