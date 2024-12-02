89e546c26da011f6041b8a4ef96631131c78bf65
swapnil <swapnil@classhubapp.com>
2024-12-02 20:52:00 +0530

    center align the 4040 page

--- app/not-found.js
+++ app/not-found.js
@@ -4,46 +4,30 @@ export default function NotFoundPage() {
   return (
     <div
       style={{
-        height: "110vh",
+        height: "100vh",
         zIndex: 999996,
         position: "absolute",
-        top: -25,
+        top: 0,
         left: 0,
         width: "100%",
         background: "white",
         display: "flex",
         alignItems: "center",
-        flexDirection: "column",
         justifyContent: "center",
+        flexDirection: "column",
       }}
-      className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 text-center mt-4 not-found"
+      className="text-center not-found"
     >
-      {/* <h1 className="text-6xl font-bold">404</h1>
-        <h2 className="text-2xl mt-2">Page Not Found</h2>
-        <p className="text-gray-600 mt-2">
-          Sorry, the page you’re looking for doesn’t exist.
-        </p>
-        <Link
-          href="/"
-          style={{ background: "var(--primary)" }}
-          className="btn btn-primary mt-4 mb-4"
-        >
-          Go Back to Home
-        </Link> */}
-      <div className="row">
-        <div className="col-md-6 align-self-center">
-          <img src="/astro.gif" alt="404" />
-        </div>
-
-        <div className="col-md-6 align-self-center">
-          <h1>404</h1>
-          <h2>UH OH! You're lost.</h2>
-          <p>
+      <div className="row w-100">
+        <div className="col-md-6 mx-auto">
+          <h1 className="display-1">404</h1>
+          <h2 className="mt-3">UH OH! You're lost.</h2>
+          <p className="mt-2">
             The page you are looking for does not exist. How you got here is a
             mystery. But you can click the button below to go back to the
             homepage.
           </p>
-          <Link href="/" className="btn green">
+          <Link href="/" className="btn btn-primary mt-4" style={{ color: "var(--primary)" }}>
             HOME
           </Link>
         </div>
