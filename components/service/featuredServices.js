fe30303daba45d7a56d64b1ca18c2377da93a14a
swapnil <swapnil@classhubapp.com>
2024-12-03 17:21:42 +0530

    updated services background image

--- components/service/featuredServices.js
+++ components/service/featuredServices.js
@@ -7,8 +7,8 @@ function FeaturedServices({ services }) {
 
     return (
         <section className="featured-products">
-            <div className="col-md-12 heading-bx left">
-                <h2 className="title-head">Featured Services</h2>
+            <div className="text-center mb-5">
+                <h3 className="fw-bold display-5">Featured Services</h3>
             </div>
 
 
@@ -17,10 +17,10 @@ function FeaturedServices({ services }) {
                     <div key={item.idServices} className="product-card">
                         <img
                             src={`${item?.media.mediaBaseUrl}/${item?.media.fileSlug}`}
-                            alt={item?.serviceTitle} className="product-image" />
+                            alt={item?.altText} className="product-image" style={{width: '300px', height: '200px'}} />
                         <div className="image-divider"></div> {/* Divider under image */}
                         <h3 className="product-name">
-                            <Link href={`/services/${item?.slug}`} className="button">
+                            <Link href={`${item?.slug}`} className="button">
                                 {item?.serviceTitle}
                             </Link>
                         </h3>
