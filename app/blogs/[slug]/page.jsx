283b8e0dffa61b9a9a4ad115ba4628f7562a991c
swapnil <swapnil@classhubapp.com>
2024-12-03 17:21:16 +0530

    blog fixed update background image from the backend

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -1,30 +1,33 @@
 import { fetchBlogs } from "@/lib/fetchBlogs";
-import Link from "next/link";
-import bannerImg from "@/public/images/banner/banner3.jpg"; // Update image import
 import BlogDescription from "@/components/Blogs/BlogDetails";
 import config from "@/config/config";
 
 export const generateMetadata = async ({ params }) => {
   var param = await params;
 
-  const response = await fetch(
-    `${config.apiBaseUrl}/blogs/metadata/${param?.slug}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": config.subscriptionId,
-      },
+  try {
+    const response = await fetch(
+      `${config.apiBaseUrl}/blogs/metadata/${param?.slug}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": config.subscriptionId,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch metadata for slug: ${param?.slug}`);
     }
-  );
 
-  if (!response.ok) {
-    console.error(`Failed to fetch metadata for slug: ${params.slug}`);
+    // Return the metadata from the API as is
+    const metadata = await response.json();
+    return metadata;
+  } catch (error) {
+    console.error(`Error fetching metadata: ${error.message}`);
+   
   }
-
-  // Return the metadata from the API as is
-  const metadata = await response.json();
-  return metadata;
 };
 
 async function fetchBlogDetails(slug) {
