3cf9c4c1de9614912731cf91a1a41e0a4a6152c5
swapnil <swapnil@classhubapp.com>
2024-12-08 21:33:26 +0530

    build fix

--- components/SocialSharing.js
+++ components/SocialSharing.js
@@ -5,14 +5,14 @@ import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import PdfDownloadDialog from './PdfDownloadModal';
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const SocialShare = ({ title, description, socialCta, type2, pdf }) => {
   
   const [currentUrl, setCurrentUrl] = useState("");
   const [isContactModalOpen, setIsContactModalOpen] = useState(false);
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+ 
 
   const toggleContactModal = () => {
     setIsContactModalOpen(!isContactModalOpen);
@@ -22,10 +22,10 @@ const SocialShare = ({ title, description, socialCta, type2, pdf }) => {
 
 
   useEffect(() => {
-    const url = `${window.location.origin}${pathname}${searchParams.toString() ? `?${searchParams}` : ""}`;
+    const url = `${window.location.origin}${pathname}`;
     setCurrentUrl(url);
       
-  }, [pathname, searchParams]);
+  }, [pathname]);
   
 
   const shareLinks = [
